16ae323dcd030ca926398f658baeaca3f20360ba
Jon Chesterfield <jonathanchesterfield@gmail.com>
2021-07-19 20:25:58 +0100

    part of opencl example, need to work out how to splice in the test IR

--- build.sh
+++ build.sh
@@ -26,7 +26,17 @@ LINK="$RDIR/bin/llvm-link"
 OPT="$RDIR/bin/opt"
 
 
-$CLANGXX -O2 -fopenmp -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=$GFX example.cpp -o example.openmp
+$CLANGXX -std=c++11 -O2 -fopenmp -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=$GFX example.cpp -o example.openmp
 
 $CLANGXX -std=c++11 -x hip example.cpp --offload-arch=$GFX -L$RDIR/lib -Wl,-rpath=$RDIR/lib  -lamdhip64  -o example.hip
+
+# $CLANGXX -x cl -Xclang -cl-std=clc++ -Dcl_khr_fp64 -Dcl_khr_fp16 -Dcl_khr_subgroups -Dcl_khr_int64_base_atomics -Dcl_khr_int64_extended_atomics -D__OPENCL__ -D__OPENCL_C_VERSION__=200 example.cpp -o example.opencl
+
+echo libamdocl64.so > amdocl64.icd
+# trailing / is necessary, as loader/linux/icd_linux just pastes the contents of that file onto the evar
+# it would not be necessary with the icd_linux source from khronos
+export OCL_ICD_VENDORS=`pwd`/
+
+# 220 is the default, accoridng to an message in cl_version.h
+$CLANGXX -std=c++11  -D__OPENCL__ -D__OPENCL_C_VERSION__=220 -DCL_TARGET_OPENCL_VERSION=220 example.cpp -I$RDIR/include/ -o example.opencl -Wno-deprecated-declarations -L$RDIR/lib -Wl,-rpath=$RDIR/lib -lOpenCL 
 
--- example.cpp
+++ example.cpp
@@ -1,30 +1,8 @@
-#include "platform_detect.hpp" // copied from hostrpc
 #include <stdint.h>
 #include <stdio.h>
 
-int setup();
-int teardown();
-
 #ifdef __HIP__
 #include "hip/hip_runtime.h"
-
-int setup() {
-  hipError_t error = hipInit(0);
-  if (error != hipSuccess) {
-    printf("hipInit failed?\n");
-    return 1;
-  }
-
-  return 0;
-}
-
-int teardown() { return 0; }
-
-#endif
-
-#ifdef _OPENMP
-int setup() { return 0; }
-int teardown() { return 0; }
 #endif
 
 #ifdef __HIP__
@@ -34,9 +12,7 @@ int teardown() { return 0; }
 #endif
 
 ANNOTATE()
-void test(uint32_t N, const char *src, char *dst);
-
-#ifdef HOSTRPC_GPU
+extern "C" void test(uint32_t N, const char *src, char *dst);
 
 #ifdef _OPENMP
 
@@ -62,8 +38,9 @@ int target(uint32_t bytes, const char *src, char *dst) {
   struct hfi {
     hfi(char *ptr) : ptr(ptr) {}
     ~hfi() { hipFree(ptr); }
+
   private:
-    char *ptr;    
+    char *ptr;
   };
 
   char *gpu_src;
@@ -71,18 +48,23 @@ int target(uint32_t bytes, const char *src, char *dst) {
 
   hipError_t error;
 
+  error = hipInit(0);
+  if (error != hipSuccess) {
+    return 1;
+  }
+
   error = hipMalloc(&gpu_src, bytes);
   if (error != hipSuccess) {
     return 1;
   }
   hfi free_src(gpu_src);
-  
+
   error = hipMalloc(&gpu_dst, bytes);
   if (error != hipSuccess) {
     return 1;
   }
   hfi free_dst(gpu_dst);
-  
+
   error = hipMemcpy(gpu_src, src, bytes, hipMemcpyHostToDevice);
   if (error != hipSuccess) {
     return 1;
@@ -92,9 +74,9 @@ int target(uint32_t bytes, const char *src, char *dst) {
   const unsigned threadsPerBlock = 64;
   size_t dynamic_shared = 0;
   hipStream_t stream = 0;
-  
-  hipLaunchKernelGGL(target_hip_global, dim3(blocks), dim3(threadsPerBlock), dynamic_shared,
-                     stream, bytes, gpu_src, gpu_dst);
+
+  hipLaunchKernelGGL(target_hip_global, dim3(blocks), dim3(threadsPerBlock),
+                     dynamic_shared, stream, bytes, gpu_src, gpu_dst);
 
   error = hipMemcpy(dst, gpu_dst, bytes, hipMemcpyDeviceToHost);
   if (error != hipSuccess) {
@@ -106,21 +88,195 @@ int target(uint32_t bytes, const char *src, char *dst) {
 
 #endif
 
+#ifdef __OPENCL__
+#include "CL/cl.h"
+#include "CL/cl_ext.h"
+
+#define CaseReturnString(x)                                                    \
+  case x:                                                                      \
+    return #x
+
+static const char *opencl_errstr(cl_int err) {
+  switch (err) {
+    CaseReturnString(CL_SUCCESS);
+    CaseReturnString(CL_DEVICE_NOT_FOUND);
+    CaseReturnString(CL_DEVICE_NOT_AVAILABLE);
+    CaseReturnString(CL_COMPILER_NOT_AVAILABLE);
+    CaseReturnString(CL_MEM_OBJECT_ALLOCATION_FAILURE);
+    CaseReturnString(CL_OUT_OF_RESOURCES);
+    CaseReturnString(CL_OUT_OF_HOST_MEMORY);
+    CaseReturnString(CL_PROFILING_INFO_NOT_AVAILABLE);
+    CaseReturnString(CL_MEM_COPY_OVERLAP);
+    CaseReturnString(CL_IMAGE_FORMAT_MISMATCH);
+    CaseReturnString(CL_IMAGE_FORMAT_NOT_SUPPORTED);
+    CaseReturnString(CL_BUILD_PROGRAM_FAILURE);
+    CaseReturnString(CL_MAP_FAILURE);
+    CaseReturnString(CL_MISALIGNED_SUB_BUFFER_OFFSET);
+    CaseReturnString(CL_COMPILE_PROGRAM_FAILURE);
+    CaseReturnString(CL_LINKER_NOT_AVAILABLE);
+    CaseReturnString(CL_LINK_PROGRAM_FAILURE);
+    CaseReturnString(CL_DEVICE_PARTITION_FAILED);
+    CaseReturnString(CL_KERNEL_ARG_INFO_NOT_AVAILABLE);
+    CaseReturnString(CL_INVALID_VALUE);
+    CaseReturnString(CL_INVALID_DEVICE_TYPE);
+    CaseReturnString(CL_INVALID_PLATFORM);
+    CaseReturnString(CL_INVALID_DEVICE);
+    CaseReturnString(CL_INVALID_CONTEXT);
+    CaseReturnString(CL_INVALID_QUEUE_PROPERTIES);
+    CaseReturnString(CL_INVALID_COMMAND_QUEUE);
+    CaseReturnString(CL_INVALID_HOST_PTR);
+    CaseReturnString(CL_INVALID_MEM_OBJECT);
+    CaseReturnString(CL_INVALID_IMAGE_FORMAT_DESCRIPTOR);
+    CaseReturnString(CL_INVALID_IMAGE_SIZE);
+    CaseReturnString(CL_INVALID_SAMPLER);
+    CaseReturnString(CL_INVALID_BINARY);
+    CaseReturnString(CL_INVALID_BUILD_OPTIONS);
+    CaseReturnString(CL_INVALID_PROGRAM);
+    CaseReturnString(CL_INVALID_PROGRAM_EXECUTABLE);
+    CaseReturnString(CL_INVALID_KERNEL_NAME);
+    CaseReturnString(CL_INVALID_KERNEL_DEFINITION);
+    CaseReturnString(CL_INVALID_KERNEL);
+    CaseReturnString(CL_INVALID_ARG_INDEX);
+    CaseReturnString(CL_INVALID_ARG_VALUE);
+    CaseReturnString(CL_INVALID_ARG_SIZE);
+    CaseReturnString(CL_INVALID_KERNEL_ARGS);
+    CaseReturnString(CL_INVALID_WORK_DIMENSION);
+    CaseReturnString(CL_INVALID_WORK_GROUP_SIZE);
+    CaseReturnString(CL_INVALID_WORK_ITEM_SIZE);
+    CaseReturnString(CL_INVALID_GLOBAL_OFFSET);
+    CaseReturnString(CL_INVALID_EVENT_WAIT_LIST);
+    CaseReturnString(CL_INVALID_EVENT);
+    CaseReturnString(CL_INVALID_OPERATION);
+    CaseReturnString(CL_INVALID_GL_OBJECT);
+    CaseReturnString(CL_INVALID_BUFFER_SIZE);
+    CaseReturnString(CL_INVALID_MIP_LEVEL);
+    CaseReturnString(CL_INVALID_GLOBAL_WORK_SIZE);
+    CaseReturnString(CL_INVALID_PROPERTY);
+    CaseReturnString(CL_INVALID_IMAGE_DESCRIPTOR);
+    CaseReturnString(CL_INVALID_COMPILER_OPTIONS);
+    CaseReturnString(CL_INVALID_LINKER_OPTIONS);
+    CaseReturnString(CL_INVALID_DEVICE_PARTITION_COUNT);
+    CaseReturnString(CL_PLATFORM_NOT_FOUND_KHR);
+  default:
+    return "Unknown OpenCL error code";
+  }
+}
+
+#define NWITEMS 64
+
+const char *source =
+    "void test(uint N, global const char *src, global char *dst);\n"
+    "kernel void target_opencl_kernel(uint N, global const char *src, "
+    "global char *dst) {\n"
+    "   global uint* p = (global uint*)dst;\n"
+    "   for (uint i = 0; i < N/4; i++) p[i] = 42;\n"
+    "   // test(N, src, dst);\n"
+    "}\n";
+
+#define ERRRET()                                                               \
+  if (err != CL_SUCCESS) {                                                     \
+    printf("opencl target L:%u failed %d [0x%x] (%s)\n", __LINE__, err, err,   \
+           opencl_errstr(err));                                                \
+    return 1;                                                                  \
+  }
+
+int target(uint32_t bytes, const char *src, char *dst) {
+  // copying from
+  // https://rocmdocs.amd.com/en/latest/Programming_Guides/Opencl-programming-guide.html#build-run-opencl
+
+  // 1. Get a platform.
+  cl_platform_id platform;
+  cl_int err;
+
+  err = clGetPlatformIDs(1, &platform, NULL);
+  ERRRET()
+
+  // 2. Find a gpu device.
+  cl_device_id device;
+  err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+  ERRRET()
+
+  // 3. Create a context and command queue on that device.
+  cl_context context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+  ERRRET()
+
+  cl_command_queue queue = clCreateCommandQueue(context, device, 0, &err);
+  ERRRET()
+
+  cl_program program =
+      clCreateProgramWithSource(context, 1, &source, NULL, &err);
+  ERRRET()
+
+  err = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+
+  if (err == CL_BUILD_PROGRAM_FAILURE) {
+    // Determine the size of the log
+    size_t log_size;
+    err = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL,
+                                &log_size);
+    ERRRET()
+
+    // Allocate memory for the log
+    char *log = (char *)malloc(log_size);
+
+    // Get the log
+    err = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, log_size,
+                                log, NULL);
+    ERRRET()
+    // Print the log
+    printf("%s\n", log);
+
+    free(log);
+    err = CL_BUILD_PROGRAM_FAILURE;
+  }
+
+  ERRRET()
+
+  cl_kernel kernel = clCreateKernel(program, "target_opencl_kernel", &err);
+  ERRRET()
+  // 5. Create a data buffer.
+  cl_mem dst_buffer =
+      clCreateBuffer(context, CL_MEM_WRITE_ONLY, bytes, NULL, NULL);
+
+  cl_mem src_buffer =
+      clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
+
+  // 6. Launch the kernel. Let OpenCL pick the local work size.
+  size_t global_work_size = NWITEMS;
+  clSetKernelArg(kernel, 0, sizeof(bytes), (void *)&bytes);
+  clSetKernelArg(kernel, 1, sizeof(src_buffer), (void *)&src_buffer);
+  clSetKernelArg(kernel, 2, sizeof(dst_buffer), (void *)&dst_buffer);
+
+  err = clEnqueueWriteBuffer(queue, src_buffer, CL_TRUE, 0, bytes, src, 0, NULL,
+                             NULL);
+  ERRRET()
+
+  printf("run kernel\n");
+
+  err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global_work_size, NULL,
+                               0, NULL, NULL);
+  ERRRET()
+
+  // 7. Look at the results via synchronous buffer map.
+  err = clEnqueueReadBuffer(queue, dst_buffer, CL_TRUE, 0, bytes, dst, 0, NULL,
+                            NULL);
+  ERRRET()
+
+  clFinish(queue);
+
+  return 0;
+}
+
 #endif
 
 void host(uint32_t N, const char *src, char *dst) { test(N, src, dst); }
 
 int main() {
 
-  if (setup() != 0) {
-    printf("setup failed\n");
-    return 1;
-  }
-
-  uint32_t src[128];
-  uint32_t dst[128];
-  uint32_t res[128];
-  uint32_t N = 128;
+  constexpr uint32_t N = 16;
+  uint32_t src[N];
+  uint32_t dst[N];
+  uint32_t res[N];
 
   for (uint32_t i = 0; i < N; i++) {
     src[i] = i;
@@ -150,10 +306,6 @@ int main() {
 
   printf("Ran successfully\n");
 
-  if (teardown() != 0) {
-    printf("teardown failed\n");
-    return 1;
-  }
   return 0;
 }
 
@@ -163,6 +315,6 @@ void test(uint32_t N, const char *src, char *dst) {
   uint32_t *d = (uint32_t *)dst;
 
   for (uint32_t i = 0; i < N / sizeof(uint32_t); i++) {
-    d[i] = 2 * s[i];
+    d[i] = 2 * __builtin_popcount(s[i]);
   }
 }
